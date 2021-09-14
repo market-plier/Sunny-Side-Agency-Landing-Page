5c2331e983915778bedfb41a17a08dae10ad8ba4
Hlib.Maksymenko <hmaksymenko@deloitte.com>
2021-09-14 21:25:22 +0300

    desktop ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  purge: [],
+ mode: 'jit',
+  purge: [
+      './src/*.html'
+  ],
+
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      'barlow': ["Barlow", 'sans-serif'],
+      'fraunces': ["Fraunces", 'serif'],
+    },
   },
   variants: {
     extend: {},
